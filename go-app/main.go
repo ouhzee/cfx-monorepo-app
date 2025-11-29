89f44053ee89682bbe8d92736030a3ee1c41a0f9
Roziqin Tahitya <ainur.rozikin@gmail.com>
2025-11-30 02:04:00 +0700

    [ozi] Add jest dependencies to lockfile,Add eslint dependency

--- go-app/main.go
+++ go-app/main.go
@@ -1,10 +1,32 @@
 package main
-import ("fmt"; "net/http"; "strings")
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.Path, "/")
-	if path == "" { fmt.Fprintf(w, "Welcome to root web") } else { fmt.Fprintf(w, "you access the %s", path) }
+	if path == "" {
+		fmt.Fprintf(w, "Welcome to root web")
+	} else {
+		fmt.Fprintf(w, "you access the %s", path)
+	}
 }
+
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	// Create a server with timeouts
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      nil, // use default handler
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
