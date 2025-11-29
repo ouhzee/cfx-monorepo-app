1762155fadf9e5bd9681b4adea6df6065ff11eea
Roziqin Tahitya <ainur.rozikin@gmail.com>
2025-11-30 02:10:36 +0700

    [ozi] Migrate ESLint config to v9

---
+++ nodejs-app/eslint.config.js
@@ -0,0 +1,21 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "semi": ["error", "always"],
+    },
+  },
+];
